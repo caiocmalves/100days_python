180eef2371ea05a01ed74c76d347a05079dcfea3
Caio Carvalho <caio.cmalves@gmail.com>
2024-03-06 18:25:44 -0300

    primeiro dia do curso

---
+++ Iniciante/day01.py
@@ -0,0 +1,6 @@
+print("Welcome to the Band Name Generator")
+
+cidade = input("What's the name of the city you grew up in? \n")
+pet = input("What's your pet name? \n")
+
+print('Your band name could be', cidade, pet, sep=' ')
