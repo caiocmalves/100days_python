ee244e4b5c8cdf67d2e0e669f710d8749423b204
Caio Carvalho de Miranda Alves <caio.carvalho1@ibm.com>
2024-03-08 10:39:40 -0300

    adicionando .lower() e o else no if das cores. Melhorando o codigo do day03

--- Iniciante/day03.py
+++ Iniciante/day03.py
@@ -1,17 +1,19 @@
 print("Welcome to Treasure Island.")
 print("Your mission is to find the trasure.")
-direction = input("You're at a cross road. Where do you want to go?\n     Type 'left' or 'right'\n")
+direction = input("You're at a cross road. Where do you want to go?\n     Type 'left' or 'right'\n").lower()
 
 if direction == "left":
-    action = input("You've come to a Lake. There is an Island in the middle of the lake.\n     Type 'wait' to wait for a boat. Type 'swim' to swim across.\n")
+    action = input("You've come to a Lake. There is an Island in the middle of the lake.\n     Type 'wait' to wait for a boat. Type 'swim' to swim across.\n").lower()
     if action == "wait":
-        color = input("You arrive at the island unharmed. There is a house with 3 doors\n    One red, one yellow and one blue. Which color do you choose?\n")
+        color = input("You arrive at the island unharmed. There is a house with 3 doors\n    One red, one yellow and one blue. Which color do you choose?\n").lower()
         if color == "yellow":
             print("You found the treasure! You win!")
         elif color == "red":
             print("It's a room full of fire. Game over.")
-        else:
+        elif:
             print("You enter a room of beasts. Game over.")
+        else:
+            print("You chose a door that doesn't exist. Game over.")
     else:
         print("You get attacked by an angry trout. Game over.")
 else:
