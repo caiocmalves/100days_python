b3b90f71713a890a3bc15dc4654e9ebb31ed8295
Caio Carvalho de Miranda Alves <caio.carvalho1@ibm.com>
2024-03-09 12:04:25 -0300

    Melhoria na lógica if de acordo com a resolução do curso e tirei o print da resposta do pc do if para fora do if diminuindo o numero de linhas

--- Iniciante/day04.py
+++ Iniciante/day04.py
@@ -3,27 +3,21 @@ import random
 user = int(input("What do you choose? Type 0 for Rock, 1 for Paper, 2 for Scissors.\n"))
 
 choice_pc = random.randint(0,2)
+print(f"The PC choice is {choice_pc}")
 
-if user == choice_pc:
-    print(f"The PC choice is {choice_pc}")
-    print("It's a draw!")
-elif user == 0 and choice_pc == 1:
-    print(f"The PC choice is {choice_pc}")
-    print("You lose.")
-elif user == 0 and choice_pc == 2:
-    print(f"The PC choice is {choice_pc}")
+if user >= 3 or user < 0:
+    print("You typed n invalid number, you lose!")
+elif user == 0 and choice_pc == 2:    
     print("You Win!")
-elif user == 1 and choice_pc == 0:
-    print(f"The PC choice is {choice_pc}")
-    print("You Win.")
-elif user == 1 and choice_pc == 2:
-    print(f"The PC choice is {choice_pc}")
-    print("You lose!")
 elif user == 2 and choice_pc == 0:
-    print(f"The PC choice is {choice_pc}")
     print("You lose.")
-elif user == 2 and choice_pc == 1:
-    print(f"The PC choice is {choice_pc}")
-    print("You Win!")
+elif user > choice_pc:
+    print("You win!")
+elif user < choice_pc:
+    print("You lose.")
+elif user == choice_pc:
+    print("It's a draw.")
+
+    
 
 #  0 for Rock, 1 for Paper, 2 for Scissors.
