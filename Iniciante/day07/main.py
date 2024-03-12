d957b63483e91a58ecfff4c5c25114b859f9c15a
Caio Carvalho <caio.cmalves@gmail.com>
2024-03-12 14:44:24 -0300

    resolução final do projeto day07 - jogo da forca

--- Iniciante/day07/main.py
+++ Iniciante/day07/main.py
@@ -6,25 +6,29 @@ tamanho_palavra = len(palavra_escolhida)
 vidas = 6
 fim_do_jogo = False
 print('Bem vindo ao jogo de FORCA! Você tem 6 vidas. Vamos começar!')
+adivinhando = []
+for i in range(len(palavra_escolhida)):
+    adivinhando.append("_")
+
 
 while fim_do_jogo == False:
-    letra = input('Adivinhe uma letra: ')
-    for i in range(len(palavra_escolhida)):
-        print("_", end=' ')
+    letra = input('Adivinhe uma letra: ').lower()
     if letra not in palavra_escolhida:
-        vidas =- 1
-        print(f'Você chutou a letra {letra}, esta letra não tem na palvra. Perdeu uma vida.')
+        vidas -= 1
+        print(f'Você chutou a letra {letra}, esta letra não tem na palavra. Perdeu uma vida.')
         print(f'Agora você tem {vidas} vidas')
+    else:
+        for i in range(len(palavra_escolhida)):
+            if letra == palavra_escolhida[i]:
+                adivinhando[i] = letra 
+    for i in range(len(adivinhando)):
+        print(adivinhando[i], end=' ')
+    print()
     
 
     if vidas == 0:
         fim_do_jogo = True
         print('Você perdeu!')
-      
-    
-    #if letra in palavra_escolhida:
-    #    print(f'Você chutou a letra {letra}, parabéns!')
-     #   for i in len(palavra_escolhida):
-      #      if palavra_escolhida[i] == letra:
-
+        print(f'A palavra era {palavra_escolhida}')
+    print()      
 
