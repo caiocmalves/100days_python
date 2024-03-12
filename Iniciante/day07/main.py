e8b0316159353f64a299f7586dc315d8096275f5
Caio Carvalho <caio.cmalves@gmail.com>
2024-03-12 15:02:59 -0300

    aplicando lógicas que não estavam antes.

--- Iniciante/day07/main.py
+++ Iniciante/day07/main.py
@@ -1,9 +1,10 @@
 import random
 from letras import word_list
 
-palavra_escolhida = random.choice(word_list)
+palavra_escolhida = random.choice(word_list).lower()
 tamanho_palavra = len(palavra_escolhida)
 vidas = 6
+letras_escolhidas = []
 fim_do_jogo = False
 print('Bem vindo ao jogo de FORCA! Você tem 6 vidas. Vamos começar!')
 adivinhando = []
@@ -13,22 +14,28 @@ for i in range(len(palavra_escolhida)):
 
 while fim_do_jogo == False:
     letra = input('Adivinhe uma letra: ').lower()
-    if letra not in palavra_escolhida:
-        vidas -= 1
-        print(f'Você chutou a letra {letra}, esta letra não tem na palavra. Perdeu uma vida.')
-        print(f'Agora você tem {vidas} vidas')
+    if letra not in letras_escolhidas:
+        if letra not in palavra_escolhida:
+            vidas -= 1
+            print(f'Você chutou a letra {letra}, esta letra não tem na palavra. Perdeu uma vida.')
+            print(f'Agora você tem {vidas} vidas')
+        else:
+            for i in range(len(palavra_escolhida)):
+                if letra == palavra_escolhida[i]:
+                    adivinhando[i] = letra 
+        for i in range(len(adivinhando)):
+            print(adivinhando[i], end=' ')
+
+        letras_escolhidas.append(letra)
+        print()
+        print()
+
+        if vidas == 0:
+            fim_do_jogo = True
+            print('Você perdeu!')
+            print(f'A palavra era {palavra_escolhida}')
+        elif '_' not in adivinhando:
+            fim_do_jogo = True
+            print('Parabéns! Você ganhou o jogo!')      
     else:
-        for i in range(len(palavra_escolhida)):
-            if letra == palavra_escolhida[i]:
-                adivinhando[i] = letra 
-    for i in range(len(adivinhando)):
-        print(adivinhando[i], end=' ')
-    print()
-    
-
-    if vidas == 0:
-        fim_do_jogo = True
-        print('Você perdeu!')
-        print(f'A palavra era {palavra_escolhida}')
-    print()      
-
+        print('Você já tentou esta letra, tente uma outra.')
