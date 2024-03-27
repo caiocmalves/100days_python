d871a5ce6363f1523ed0415913295c34d521a5b1
Caio Carvalho <caio.cmalves@gmail.com>
2024-03-27 19:07:32 -0300

    Inicio da resolução do dia11

---
+++ Iniciante/day11/main.py
@@ -0,0 +1,50 @@
+import random
+
+
+
+def iniciarJogo():
+    dealer.append(random.choice(cards))
+    dealer.append(random.choice(cards))
+    player.append(random.choice(cards))
+    player.append(random.choice(cards))
+    definirCarta(player)
+    definirCarta(dealer)
+    print(f"Suas cartas: {player}, sua pontuação atual: {sum(player)}")
+    print(f"Primeira carta do dealer: {dealer[0]}")
+
+def definirCarta(lista):
+    for i in range(len(lista)):
+        if lista[i] == "valete" or lista[i] == "dama" or lista[i] == "rei":
+            lista[i] = 10
+        elif lista[i] == "as":
+            lista[i] = 11
+            if sum(lista) > 21:
+                lista[i] = 1    
+
+def conferirJogo(player,dealer):
+    return 
+
+
+cards = [2,3,4,5,6,7,8,9,10,"valete","dama","rei","as"]
+dealer = []
+player = []
+play = "sim"
+maisUmaCarta = 'não'
+play = input("Você quer jogar Blackjack? Digite 'sim' ou 'não':  ").lower().strip()
+
+
+while play == "sim":
+    iniciarJogo()
+
+    maisUmaCarta = input("Digite 'sim' para pegar mais uma carta ou digite 'não' para passar a vez: ").lower().strip()
+    if maisUmaCarta == "sim":
+        player.append(random.choice(cards))
+        definirCarta(player)
+        if sum(player) > 21:
+            print(f"Suas cartas: {player}, sua pontuação atual: {sum(player)}")
+            print(f"Primeira carta do dealer: {dealer[0]}")
+            print(f"Sua mão final: {player}, sua pontuação final: {sum(player)}")
+            print(f"Mão final do dealer: {dealer}")
+            print("Você passou de 21. Você perdeu! :()")
+
+    play = input("Você quer jogar Blackjack? Digite 'sim' ou 'não':  ").lower().strip()            
