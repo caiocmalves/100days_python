0723e7c99107c3d258cfa0b597f6e0e24d7b5ef6
Caio Carvalho <caio.cmalves@gmail.com>
2024-03-27 19:25:36 -0300

    sequencia da implementação, adicionado conclusão de jogo

--- Iniciante/day11/main.py
+++ Iniciante/day11/main.py
@@ -9,42 +9,65 @@ def iniciarJogo():
     player.append(random.choice(cards))
     definirCarta(player)
     definirCarta(dealer)
+    
+def contagemMao(player,dealer):
     print(f"Suas cartas: {player}, sua pontuação atual: {sum(player)}")
     print(f"Primeira carta do dealer: {dealer[0]}")
+
 
 def definirCarta(lista):
     for i in range(len(lista)):
         if lista[i] == "valete" or lista[i] == "dama" or lista[i] == "rei":
             lista[i] = 10
-        elif lista[i] == "as":
+    for i in range(len(lista)):
+        if lista[i] == "as":
             lista[i] = 11
             if sum(lista) > 21:
                 lista[i] = 1    
 
 def conferirJogo(player,dealer):
-    return 
+    if sum(player) > 21:
+            print(f"Sua mão final: {player}, sua pontuação final: {sum(player)}")
+            print(f"Mão final do dealer: {dealer}")
+            print("Você passou de 21. Você perdeu! :()")
+    elif sum(dealer) > 21 and sum(player) <= 21:
+            print(f"Sua mão final: {player}, sua pontuação final: {sum(player)}")
+            print(f"Mão final do dealer: {dealer}")
+            print("O dealer passou de 21. Você ganhou! :))")
+    elif sum(player) <= 21 and sum(dealer) < sum(player):
+            print(f"Sua mão final: {player}, sua pontuação final: {sum(player)}")
+            print(f"Mão final do dealer: {dealer}")
+            print("Parabéns, Você ganhou! :))")
+         
 
 
 cards = [2,3,4,5,6,7,8,9,10,"valete","dama","rei","as"]
 dealer = []
 player = []
 play = "sim"
-maisUmaCarta = 'não'
+maisUmaCarta = 'sim'
 play = input("Você quer jogar Blackjack? Digite 'sim' ou 'não':  ").lower().strip()
 
 
 while play == "sim":
     iniciarJogo()
+    contagemMao(player,dealer)
+    print()
+    while maisUmaCarta == "sim":
+        maisUmaCarta =  input("Digite 'sim' para pegar mais uma carta ou digite 'não' para passar a vez: ").lower().strip()
+        if maisUmaCarta == "sim":
+            player.append(random.choice(cards))
+            definirCarta(player)
+            print()
+            contagemMao(player,dealer)
+            conferirJogo(player,dealer)
+            
 
-    maisUmaCarta = input("Digite 'sim' para pegar mais uma carta ou digite 'não' para passar a vez: ").lower().strip()
-    if maisUmaCarta == "sim":
-        player.append(random.choice(cards))
-        definirCarta(player)
-        if sum(player) > 21:
-            print(f"Suas cartas: {player}, sua pontuação atual: {sum(player)}")
-            print(f"Primeira carta do dealer: {dealer[0]}")
-            print(f"Sua mão final: {player}, sua pontuação final: {sum(player)}")
-            print(f"Mão final do dealer: {dealer}")
-            print("Você passou de 21. Você perdeu! :()")
+
+        else:
+            definirCarta(player)
+            contagemMao(player,dealer)
+            conferirJogo(player,dealer)
+        maisUmaCarta = "não"
 
     play = input("Você quer jogar Blackjack? Digite 'sim' ou 'não':  ").lower().strip()            
